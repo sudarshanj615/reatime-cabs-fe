91bac0bb8f485ad35b12a4d59b21f3d4b566384b
Abhishek Alkari <you@example.com>
2026-05-25 11:18:40 +0530

    changes in create new ride page

--- components/booking/RideSearchBar.tsx
+++ components/booking/RideSearchBar.tsx
@@ -1,3 +1,8 @@
 export function RideSearchBar() {
-  return <input className="w-full min-h-[54px] border border-[#eadfbb] rounded-[10px] p-[10px] bg-[rgba(255,255,255,0.2)] text-[#080808] mt-[5px] outline-none focus:border-[#ffd232] focus:shadow-[0_0_0_3px_rgba(248,189,16,0.22)] max-[520px]:w-full" placeholder="Search nearby place, airport, station, or landmark" />;
+  return(
+    <>
+    <label>Search Nearby Places and Landmarks</label>
+    <input className="w-full min-h-[54px] border border-[#eadfbb] rounded-[10px] p-[10px] bg-[rgba(255,255,255,0.2)] text-[#080808] mt-[5px] outline-none focus:border-[#ffd232] focus:shadow-[0_0_0_3px_rgba(248,189,16,0.22)] max-[520px]:w-full" placeholder="Search nearby place, airport, station, or landmark" />
+    </>
+  ) 
 }
